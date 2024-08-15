bbfe656dcc3915f9df205e0af9ab617dffeed6a6
Irving <a223216739@unison.mx>
2024-08-14 22:06:07 -0700

    Cuenta cuantos asentamientos urbanos y cuantos rurales hay en Hermosillo

--- src/main/java/mx/unison/Main.java
+++ src/main/java/mx/unison/Main.java
@@ -4,9 +4,42 @@ package mx.unison;
 y cantos a asentamientos rurales
  */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        String archivo = "codigos_postales.csv";
+        int contadorUrbanos = 0;
+        int contadorRurales = 0;
 
-        System.out.println("Hello world!");
+        try (BufferedReader BR = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+
+            // Leer la primera linea (encabezado) y descartarla
+            BR.readLine();
+
+            while ((linea = BR.readLine()) != null) {
+                String[] campos = linea.split(",");
+                String tipoAsentamiento = campos[2].trim();
+
+                if (tipoAsentamiento.equalsIgnoreCase("Urbano")) {
+                    contadorUrbanos++;
+                }
+                else
+                    if (tipoAsentamiento.equalsIgnoreCase("Rural")) {
+                    contadorRurales++;
+                }
+            }
+
+            // Mostrar los resultados
+            System.out.println("Cantidad de asentamientos urbanos: " + contadorUrbanos);
+            System.out.println("Cantidad de asentamientos rurales: " + contadorRurales);
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
